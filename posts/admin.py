d6a63cd74fc0752eec2709a2a550dff2378db186
Pulkit Pansari <pansari.pulkit13@gmail.com>
2021-01-24 15:01:35 +0000

    update homepage

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import User, Post, PostComment, PostLike, UserFollow
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display= ('title', 'user', 'description', 'created_at', 'modified_at')
+
+
+class PostLikeAdmin(admin.ModelAdmin):
+    list_display= ('post', 'user')
+
+class PostCommentAdmin(admin.ModelAdmin):
+    list_display= ('post', 'user', 'text', 'commented_at')
+
+
 admin.site.register(UserFollow)
-admin.site.register(Post)
-admin.site.register(PostLike)
-admin.site.register(PostComment)
+admin.site.register(Post,PostAdmin)
+admin.site.register(PostLike, PostLikeAdmin)
+admin.site.register(PostComment, PostCommentAdmin)
--- posts/views.py
+++ posts/views.py
@@ -28,8 +28,8 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreateForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.set_password(form.cleaned_data.get('password'))
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data.get('password1'))
             user.save()
             login(request, user)
             return redirect('posts:accounts_main', username=user.username)
